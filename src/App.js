5cac519b548b6fe97b82998b1741069bf0c225ad
Mwai Joseph <joesoftmwai@gmail.com>
2022-02-25 07:47:17 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Topbar from './components/topbar/Topbar';
+
+function App() {
+  return (
+    <div className="App">
+      <Topbar/>
+    </div>
+  );
+}
+
+export default App;
