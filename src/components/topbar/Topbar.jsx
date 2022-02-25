de8db99c1dcae162822d5622ad89f40b37a1e90d
Mwai Joseph <joesoftmwai@gmail.com>
2022-02-25 15:38:18 +0300

    created a stick-(top) topbar

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import React from 'react';
+import './Topbar.css';
+import {Notifications, Language, Settings} from '@mui/icons-material';
+
 
 const Topbar = () => {
   return (
-    <div>Topbar</div>
+    <div className='topbar'>
+      <div className="topbarWrapper">
+        <div className="topLeft">
+          <span className="logo">Admin Dash</span>
+        </div>
+        <div className="topRight">
+          <div className="topbarIconContainer">
+            <Notifications />
+            <span className="topIconBadge">2</span>
+          </div>
+          <div className="topbarIconContainer">
+            <Language />
+            <span className="topIconBadge">2</span>
+          </div>
+          <div className="topbarIconContainer">
+            <Settings />
+          </div>
+          <img src="https://cdn.pixabay.com/photo/2020/07/14/13/07/icon-5404125_1280.png" alt="profile" className="topAvatar" />
+        </div>
+      </div>
+    </div>
   )
 }
 
